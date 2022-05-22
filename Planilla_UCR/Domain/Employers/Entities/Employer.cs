9d26f1b263b306724f0a9f7350cb88bcd4222250
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-05-22 08:47:01 -0600

    Add Employer to Domain

---
+++ Planilla_UCR/Domain/Employers/Entities/Employer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Employers.Entities
+{
+    public class Employer
+    {
+        public String Email { get; }
+
+        public Employer(String email)
+        {
+            Email = email;
+        }
+    }
+}
