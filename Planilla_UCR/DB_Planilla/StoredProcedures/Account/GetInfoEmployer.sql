399f5f2655baeacd09fb14bddb94240c21c833ac
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-05-22 09:33:18 -0600

    Added Stored procedure to retrieve employer information

---
+++ Planilla_UCR/DB_Planilla/StoredProcedures/Account/GetInfoEmployer.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE GetInfoEmployer(@EmailEmployer varchar(255))
+AS
+BEGIN
+	SELECT Employer.Email, Person.Name, Person.LastName1, Person.LastName2, Person.SSN, Person.BankAccount, Person.Adress, Person.PhoneNumber
+FROM Employer JOIN  Person ON Employer.Email = Person.Email where Employer.Email = @EmailEmployer
+END
