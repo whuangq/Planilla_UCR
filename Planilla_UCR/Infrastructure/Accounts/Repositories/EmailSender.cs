85bfb1d82f9870295b0ba4a4c22df2077f59cb30
LeonelCamposM <leonelscamposm@gmail.com>
2022-05-24 23:19:22 -0600

    add page interactions

--- Planilla_UCR/Infrastructure/Accounts/Repositories/EmailSender.cs
+++ Planilla_UCR/Infrastructure/Accounts/Repositories/EmailSender.cs
@@ -2,6 +2,9 @@
 using SendGrid.Helpers.Mail;
 using System;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Infrastructure.Accounts.Repositories
 {
@@ -9,19 +12,73 @@ namespace Infrastructure.Accounts.Repositories
     {
         public EmailSender(){}
 
+        
 
         // Must install PM> Install-Package SendGrid
         public async Task Execute(string message, string receiver)
         {
-            var client = new SendGridClient("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");
-            var from = new EmailAddress("stevenleonel22201@gmail.com", "Planilla_UCR");
+            EncryptionHelper encryptionHelper = new EncryptionHelper();
+            string a = "QcXhdv4TLgn6xKN79GKqRSMOb3oNvEXXmAMMA0HotgBrh9cBkOR4E0S1Ct0QBPvp29T57umzIFCiNYVrcYw9L2vvPrHEfkcjj83BHa5Nrf2X0nzTcnlPUnfyTIh1Aq99Z0tc0vJqkM93emLQEGnCYFkz7ou6Ny4CPA587IHOLhLvaGQpd/ow52Hlf1zK2WwM";
+            string value = encryptionHelper.Decrypt(a);
+            var client = new SendGridClient(a);
+            var from = new EmailAddress("nayeriazofeifa3003@gmail", "Planilla_UCR");
             var subject = "Account confirmation";
             var to = new EmailAddress(receiver, "");
             var plainTextContent = "";
             var htmlContent = "<strong>"+ message + "</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            msg.SetClickTracking(false, false);
             var response = await client.SendEmailAsync(msg);
             Console.WriteLine(response);
         }
     }
+
+    public class EncryptionHelper
+    {
+        public EncryptionHelper() { }
+        public string Encrypt(string clearText)
+        {
+            string EncryptionKey = "abc123";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        public string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "abc123";
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
 }
+
