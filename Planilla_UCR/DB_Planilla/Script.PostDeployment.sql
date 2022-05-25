d81598a0a4793b4e8136bebd18b3b850c0e0e285
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-05-25 10:28:16 -0600

    Fixed details

--- Planilla_UCR/DB_Planilla/Script.PostDeployment.sql
+++ Planilla_UCR/DB_Planilla/Script.PostDeployment.sql
@@ -1,1 +1,15 @@
-﻿
+﻿use [DB_Planilla]
+
+INSERT INTO Person
+VALUES('jeremy@ucr.ac.cr',
+'Jeremy',
+'Vargas',
+'Artavia',
+5454454,
+'40234020012',
+'San José, Costa Rica',
+'62571204'
+)
+
+INSERT INTO Employer
+VALUES('jeremy@ucr.ac.cr')
