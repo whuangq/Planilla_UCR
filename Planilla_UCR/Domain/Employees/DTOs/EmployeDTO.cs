1b91fa0894456aa4c07443cee3ca4ac2236e1927
LeonelCamposM <leonelscamposm@gmail.com>
2022-06-01 17:54:02 -0600

    apply view retroalimentations

--- Planilla_UCR/Domain/Employees/DTOs/EmployeDTO.cs
+++ Planilla_UCR/Domain/Employees/DTOs/EmployeDTO.cs
@@ -5,7 +5,6 @@ namespace Domain.Employees.DTOs
     public class EmployeeDTO
     {
         public String Email { get; set; }
-
         public EmployeeDTO(String email)
         {
             Email = email;
