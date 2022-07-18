b4778f2ffdd1c451643c2c7dbeae8fac4821c175
Wendy Ortiz <52548500+Wendy-Ortiz@users.noreply.github.com>
2022-07-18 16:50:40 -0600

    Pcs 96 notify employees (#77)
    * Add new method for payment entity
    
    * Create a new view for report 5
    
    * Craete a new object for employee history view
    
    * Add index
    
    * Add button and function for download an excel file
    
    * Create new models for the excel
    
    * Add new attribute for a filter function
    
    * New filters for search
    
    * New attributes for employee Payment History
    
    * Changes in the model foe employee list history
    
    * Functions to complete the columns
    
    * Add format changes
    
    * Benefits column method
    
    * Possible GetTotalSocialCharges method
    
    * Some extra changes for the main
    
    * Test for the branch
    
    * Fix details for pr
    
    * Complete columns
    
    * Fix filters
    
    * Fix filters functionality
    
    * change view for email dialog
    
    * Final view for dialog information
    
    * Temporal send email
    
    * Filnal details for pr
    
    * change email password
    
    * Send email to the employeer
    
    * Changes in the test for pr
    
    * delete extra code
    
    * Justify text
    
    * Justify content
    
    * Change name in the email

---
+++ Planilla_UCR/TestProject1/Application/EmployerAgreementsService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using LanguageExt;
+using Application.Agreements.Implementations;
+using Application.Agreements;
+using Domain.Agreements.Repositories;
+using Domain.Agreements.Entities;
+
+namespace Tests.Application
+{
+    public class GetEmployerAgreementsTest
+    {
+
+        List<Agreement> agreementsList = new List<Agreement>
+        {
+            new Agreement("leonel@ucr.ac.cr", "mau@ucr.ac.cr", "Projecto 1",  DateTime.Now.AddDays(1), "Medio tiempo", 1500, DateTime.Now.AddDays(15), 1, ""),
+            new Agreement("leonel@ucr.ac.cr", "mau@ucr.ac.cr", "Projecto 2",  DateTime.Now.AddDays(1), "Tiempo completo", 1300, DateTime.Now.AddDays(25), 1, ""),
+            new Agreement("leonel@ucr.ac.cr", "mau@ucr.ac.cr", "Projecto ",  DateTime.Now.AddDays(1),  "Servicios profesionales", 1200, DateTime.Now.AddDays(15), 1, ""),
+        };
+
+        private String _employerEmail = "mau@ucr.ac.cr";
+
+
+        [Fact]
+        public async Task GetEmployerAgreements()
+        {
+            //arrange
+            var mockAgreementRepository = new Mock<IAgreementRepository>();
+            var agreementService = new AgreementService(mockAgreementRepository.Object);
+            mockAgreementRepository.Setup(repo => repo.GetEmployerAgreements("mau@ucr.ac.cr")).ReturnsAsync(agreementsList.Where(
+                e => e.EmployerEmail == _employerEmail));
+
+            //act
+            var agreementTest = agreementService.GetEmployerAgreements(_employerEmail);
+
+            //assert
+            mockAgreementRepository.Verify(repo => repo.GetEmployerAgreements(_employerEmail), Times.Once);
+            agreementTest.Count().Should().Equals(3);
+        }
+    }
+}
--- Planilla_UCR/TestProject1/Application/PaymentServiceTest.cs
+++ Planilla_UCR/TestProject1/Application/PaymentServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Application.Payments.Implementations;
 using Moq;
